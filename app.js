f98c3eec46b6c6e39cc5a9faa22d2460d52acbaf
unknown <levi.millikin@gmail.com>
2023-01-11 12:22:07 -0800

    fixed random page server render

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ app.use("/api", allRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/api", authRoutes);
 
+app.use((req, res, next) => {
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/public/index.html");
+});
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
